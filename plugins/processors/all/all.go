101ae9cab19c5ebef816a19c98b8d6b27d745106
Alexander Shepelin <brightcor@gmail.com>
2018-02-27 00:08:06 +0300

    Add regex processor plugin

--- plugins/processors/all/all.go
+++ plugins/processors/all/all.go
@@ -4,4 +4,5 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/processors/override"
 	_ "github.com/influxdata/telegraf/plugins/processors/printer"
 	_ "github.com/influxdata/telegraf/plugins/processors/lowercase"
+	_ "github.com/influxdata/telegraf/plugins/processors/regex"
 )
